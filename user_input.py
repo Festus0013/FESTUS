c96116c9801a47558363714b2a5973f1e4ae4aff
Festus0013 <160476963+Festus0013@users.noreply.github.com>
2024-02-29 23:10:33 +0100

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Prompting user for name, age, and location
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+# Printing personalized message
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
